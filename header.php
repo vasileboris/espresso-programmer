25b7261bd8e92ab4cd08e79269dbbd01727a41c9
vasile boris <boris@espressoprogrammer.com>
2017-11-11 21:33:42 +0200

    Style header with css

--- header.php
+++ header.php
@@ -1,18 +1,19 @@
-<header role='banner'>
-    <div>
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( bloginfo( 'name' ) ); ?>" rel="home">
+<header role="banner" class="header">
+    <div class="blog-header">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+           title="<?php echo esc_attr( bloginfo( 'name' ) ); ?>"
+           class="blog-home" rel="home">
             <img src="<?php echo esc_url( get_header_image() ); ?>"
-                 height="<?php echo absint( get_custom_header()->height ); ?>"
-                 width="<?php echo absint( get_custom_header()->width ); ?>"
-                 alt="<?php echo esc_attr( bloginfo( 'name' ) ); ?>" />
+                 alt="<?php echo esc_attr( bloginfo( 'name' ) ); ?>"
+                 class="blog-logo" />
         </a>
+        <div class="blog-name">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <?php bloginfo( 'name' ); ?>
+            </a>
+        </div>
+        <div class="blog-description"><?php bloginfo( 'description' ); ?></div>
     </div>
-    <div>
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-            <?php bloginfo( 'name' ); ?>
-        </a>
-    </div>
-    <div><?php bloginfo( 'description' ); ?></div>
     <?php wp_nav_menu(
         array(
             'theme_location' => 'primary',
