112802b4f0628218c75927624e245e76f5b83a7a
vasile boris <boris@espressoprogrammer.com>
2017-11-13 20:54:37 +0200

    Add support for future translations

--- navigation-single.php
+++ navigation-single.php
@@ -2,13 +2,13 @@
     <nav>
         <div>
             <?php if( get_next_post_link() ) : ?>
-                <div>Next</div>
+                <div><?php _e( 'Next', 'espresso-programmer' ); ?></div>
                 <div><?php next_post_link( '%link' ); ?></div>
             <?php endif; ?>
         </div>
         <div>
             <?php if( get_previous_post_link() ) : ?>
-                <div>Previous</div>
+                <div><?php _e( 'Previous', 'espresso-programmer' ); ?></div>
                 <div><?php previous_post_link( '%link' ); ?></div>
             <?php endif; ?>
         </div>
--- navigation.php
+++ navigation.php
@@ -2,12 +2,12 @@
     <nav>
         <div>
             <?php if( get_previous_posts_link() ) :
-                previous_posts_link( 'Newer Entries' );
+                previous_posts_link( __( 'Newer Posts', 'espresso-programmer' ) );
             endif; ?>
         </div>
         <div>
             <?php if( get_next_posts_link() ) :
-                next_posts_link( 'Older Entries' );
+                next_posts_link( __( 'Older Posts', 'espresso-programmer' ) );
             endif; ?>
         </div>
     </nav>
