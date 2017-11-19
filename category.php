03b98c790f2b94014931aedffb77397dcc0137fe
vasile boris <boris@espressoprogrammer.com>
2017-11-19 19:10:45 +0200

    Add support for category and tag navigation

---
+++ category.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<?php get_sidebar('menu'); ?>
+
+<section class="posts" role="main">
+
+    <?php if ( have_posts() ) : ?>
+
+        <header class="post-info">
+            <h1><?php printf( __( 'Category Archives: %s', 'espresso-programmer' ), single_cat_title( '', false ) ); ?></h1>
+
+            <?php if ( category_description() ) : // Show an optional category description ?>
+                <div class="post-info-meta"><?php echo category_description(); ?></div>
+            <?php endif; ?>
+        </header>
+
+        <?php while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'content' ); ?>
+        <?php endwhile; ?>
+
+        <?php get_template_part('navigation'); ?>
+
+    <?php else : ?>
+        <?php get_template_part( 'content', 'none' ); ?>
+    <?php endif; ?>
+
+</section>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
---
+++ tag.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<?php get_sidebar('menu'); ?>
+
+<section class="posts" role="main">
+
+    <?php if ( have_posts() ) : ?>
+
+        <header class="post-info">
+            <h1><?php printf( __( 'Tag Archives: %s', 'espresso-programmer' ), single_tag_title( '', false ) ); ?></h1>
+
+            <?php if ( tag_description() ) : // Show an optional tag description ?>
+                <div class="post-info-meta"><?php echo tag_description(); ?></div>
+            <?php endif; ?>
+        </header>
+
+        <?php while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'content' ); ?>
+        <?php endwhile; ?>
+
+        <?php get_template_part('navigation'); ?>
+
+    <?php else : ?>
+        <?php get_template_part( 'content', 'none' ); ?>
+    <?php endif; ?>
+
+</section>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
