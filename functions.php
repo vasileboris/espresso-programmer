a41000851e8b175ff9668331a9410457223f2718
vasile boris <boris@espressoprogrammer.com>
2017-11-21 08:39:28 +0200

    Replace aside with div

--- functions.php
+++ functions.php
@@ -76,8 +76,8 @@ function espresso_programmer_widgets_init() {
     register_sidebar( array(
         'name'          => 'Right sidebar',
         'id'            => 'espresso_programmer_right',
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</aside>',
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</div>',
         'before_title'  => '<h2 class="widget-title">',
         'after_title'   => '</h2>',
     ) );
