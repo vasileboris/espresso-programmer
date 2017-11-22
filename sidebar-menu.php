b3a6c7ed8f6d84e7e2d8be4c61fd2a39db969a51
vasile boris <boris@espressoprogrammer.com>
2017-11-22 08:48:22 +0200

    Remove sidebar-menu

--- sidebar-menu.php
+++ sidebar-menu.php
@@ -1,12 +1,10 @@
-<nav class="sidebar-menu" role="menu">
-    <?php wp_nav_menu(
-        array(
-            'theme_location' => 'primary',
-            'container'       => 'nav',
-            'container_id'    => 'menu-primary',
-            'container_class' => 'menu',
-            'menu_id'         => 'menu-primary-items',
-            'menu_class'      => 'menu-items',
-            'depth' => 1
-        )); ?>
-</nav>
+<?php wp_nav_menu(
+    array(
+        'theme_location' => 'primary',
+        'container'       => 'nav',
+        'container_id'    => 'menu-primary',
+        'container_class' => 'menu',
+        'menu_id'         => 'menu-primary-items',
+        'menu_class'      => 'menu-items',
+        'depth' => 1
+    )); ?>
