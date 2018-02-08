3fde9b21e4420270308628a573c9978a70e903e8
vasile boris <boris@espressoprogrammer.com>
2018-02-08 08:37:48 +0200

    Do not escape get_search_query

--- searchform.php
+++ searchform.php
@@ -6,10 +6,9 @@
  * @since Espresso Programmer 1.0
  */
 ?>
-
 <form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 	<input type="text" class="search-text" name="s"
-		   value="<?php echo esc_attr( get_search_query() ); ?>"
+		   value="<?php echo get_search_query(); ?>"
 		   placeholder="<?php esc_attr_e( 'Search &hellip;', 'espresso-programmer' ); ?>" />
 	<input type="submit" class="search-submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'espresso-programmer' ); ?>" />
 </form>
